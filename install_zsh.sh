89ce4562a9712b44e87a5236f1c5fe7583da358e
John Galt <galt_john@protonmail.com>
2024-08-04 11:31:23 -0300

    Script to install zsh

---
+++ install_zsh.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt upgrade -y
+
+# Instalar e configurar ZSH
+sudo apt install zsh -y
+chsh -s /bin/zsh
+zsh
+
+# Instalar Oh-my-zsh! -> https://ohmyz.sh/
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Instalar Spaceship Prompt
+# https://github.com/dallas-prompt/dallas-prompt
+git clone https://github.com/dallas-prompt/dallas-prompt.git "$ZSH_CUSTOM/themes/dallas-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/dallas.zsh-theme" "$ZSH_CUSTOM/themes/dallas.zsh-theme"
+
+# Mudar ~/.zshrc -> ZSH_THEME="dallas"
+
+# Instalar Zsh Autosuggestions
+# https://github.com/zsh-users/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Instalar Zsh Syntax Highlighting
+# https://github.com/zsh-users/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Mudar plugins em .zshrc
+# plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
+
+# Font optional (https://github.com/pdf/ubuntu-mono-powerline-ttf)
+mkdir -p ~/.fonts
+git clone https://github.com/pdf/ubuntu-mono-powerline-ttf.git ~/.fonts/ubuntu-mono-powerline-ttf
+fc-cache -vf
